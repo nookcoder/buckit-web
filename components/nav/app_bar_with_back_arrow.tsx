c2958efc9281dd53a6c11ddb34066304667384e7
wook <gus5427@naver.com>
2022-08-20 12:05:01 +0900

    DESIGN : 뒤로가기 앱바 생성

---
+++ components/nav/app_bar_with_back_arrow.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import appbarStyles from '../../styles/nav/AppBar.module.scss';
+import { IconButton } from '@mui/material';
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+import { useRouter } from 'next/router';
+
+interface AppBarWithBackArrowProps {
+  title?: string;
+}
+
+const AppBarWithBackArrow = ({ title }: AppBarWithBackArrowProps) => {
+  const router = useRouter();
+  const onClick: React.MouseEventHandler<HTMLButtonElement> = () => {
+    router.back();
+  };
+  return (
+    <>
+      <div className={appbarStyles.container}>
+        <IconButton onClick={onClick}>
+          <NavigateBeforeIcon fontSize={'large'} />
+        </IconButton>
+
+        <h3>{title}</h3>
+
+        <div />
+      </div>
+      <div className={appbarStyles.blank_box}></div>
+    </>
+  );
+};
+
+export default AppBarWithBackArrow;
