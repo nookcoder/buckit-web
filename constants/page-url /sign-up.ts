641da903076537feb1f04a13720bd358dc3dd011
wook <gus5427@naver.com>
2022-09-14 17:08:39 +0900

    ADD : 회원가입 관련 PAGE URL Constants 추가

---
+++ constants/page-url /sign-up.ts
@@ -0,0 +1,13 @@
+const SIGNUP_PHONE = '/user/sign-up';
+const SIGNUP_EMAIL = '/user/sign-up/email';
+const SIGNUP_TERMS = '/user/sign-up/terms';
+const SIGNUP_PASSWORD = '/user/sign-up/password';
+const SIGNUP_COMPLETION = '/user/sign-up/completion';
+
+export {
+  SIGNUP_EMAIL,
+  SIGNUP_PASSWORD,
+  SIGNUP_TERMS,
+  SIGNUP_PHONE,
+  SIGNUP_COMPLETION,
+};
