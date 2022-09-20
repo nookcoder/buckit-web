c2024749aa4e08a244e832cb26e6d1d0e80b536e
wook <gus5427@naver.com>
2022-09-20 14:13:10 +0900

    ADD : 환경 변수 추가

--- next.config.js
+++ next.config.js
@@ -4,9 +4,18 @@ const path = require('path');
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['buckit-prod.s3.ap-northeast-2.amazonaws.com'],
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
-  }
-}
+  },
+  env: {
+    IMP_CERTIFICATION_CODE: process.env.IMP_CERTIFICATION_CODE,
+    MERCHANT_UID: process.env.MERCHANT_UID,
+    BASE_URL: process.env.BASE_URL,
+    REFRESH_COOKIE_KEY: process.env.REFRESH_COOKIE_KEY,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
