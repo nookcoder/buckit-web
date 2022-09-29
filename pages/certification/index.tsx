3bfdff4061d5a7fc6c61e3991738558a6ee38307
wook <gus5427@naver.com>
2022-09-29 15:36:33 +0900

    FIX : 인증 오류 수정

--- pages/certification/index.tsx
+++ pages/certification/index.tsx
@@ -48,7 +48,7 @@ const PhoneCertification = () => {
           // param
           merchant_uid: process.env.MERCHANT_UID, // 주문 번호
           m_redirect_url: process.env.BASE_URL,
-          popup: false, // PC환경에서는 popup 파라메터가 무시되고 항상 true 로 적용됨
+          popup: true, // PC환경에서는 popup 파라메터가 무시되고 항상 true 로 적용됨
           phone: createUser.phoneNumber,
         },
         async (rsp: IMPResponse) => {
